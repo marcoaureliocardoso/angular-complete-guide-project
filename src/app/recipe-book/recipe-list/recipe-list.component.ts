c0a6aaeaec70def28db817b22ce918eb5ff1b1e4
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2023-12-23 04:18:29 -0300

    feat(recipe book): make the partial list and detail

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://img.rawpixel.com/s3fs-private/rawpixel_images/website_content/a019-jakubk-0718-carrot-cake-in-a-cafe.jpg?w=1200&h=1200&fit=clip&crop=default&dpr=1&q=75&vib=3&con=3&usm=15&cs=srgb&bg=F4F4F3&ixlib=js-2.2.1&s=1224999697a6716784f90171e31c8de6'
+    ),
+    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://lobsterfrommaine.com/wp-content/uploads/fly-images/1386/20210503-MLMC-Spring-Radish-Pea-Lobster-Salad-Labneh2874-1-530x380-c.jpg'),
+  ];
 }
