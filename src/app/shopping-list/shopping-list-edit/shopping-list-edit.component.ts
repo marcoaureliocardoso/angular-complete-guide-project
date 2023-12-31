166ecf99b89556704e21e2aed6da29eeeb3ba8a3
Marco Aurelio Cardoso <marcoaurelio.cardoso@gmail.com>
2023-12-31 20:21:17 -0300

    feat(shopping list): make basic add item feature

--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Ingredient } from '../../shared/ingredient';
 
 @Component({
   selector: 'app-shopping-list-edit',
   templateUrl: './shopping-list-edit.component.html',
-  styleUrl: './shopping-list-edit.component.css'
+  styleUrl: './shopping-list-edit.component.css',
 })
 export class ShoppingListEditComponent {
+  @ViewChild('nameInput') public nameInputRef: ElementRef;
+  @ViewChild('amountInput') public amountInputRef: ElementRef;
+  @Output() public itemCreated: EventEmitter<Ingredient> = new EventEmitter<Ingredient>();
 
+  createItem() {
+    const name: string = this.nameInputRef.nativeElement.value;
+    const amount: number = this.amountInputRef.nativeElement.value;
+    this.itemCreated.emit(new Ingredient(name, amount));
+  }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -4,11 +4,15 @@ import { Ingredient } from '../shared/ingredient';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrl: './shopping-list.component.css'
+  styleUrl: './shopping-list.component.css',
 })
 export class ShoppingListComponent {
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
   ];
+
+  onItemCreated(item: Ingredient) {
+    this.ingredients.push(item);
+  }
 }
